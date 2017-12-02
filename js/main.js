450fb63a846958491fb2db07fcb8df04f828c19e
bangxingni <bangxingni@163.com>
2017-12-02 15:37:32 +0800

    Site updated: 2017-12-02 15:37:26

--- js/main.js
+++ js/main.js
@@ -27,66 +27,63 @@
  animate();
  function init() {
 
-    container = document.createElement( 'div' );
-    battery.appendChild( container );
+ 	container = document.createElement( 'div' );
+ 	battery.appendChild( container );
 
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-    camera.position.z = 35;
-
+ 	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+ 	camera.position.z = 35;
     // scene
-
     scene = new THREE.Scene();
-
    // camera.lookAt(scene.position);
-    var controls = new THREE.OrbitControls(camera);
+   var controls = new THREE.OrbitControls(camera);
 
-    var ambient = new THREE.AmbientLight( 0x444444 );
-    scene.add( ambient );
+   var ambient = new THREE.AmbientLight( 0x444444 );
+   scene.add( ambient );
 
-    directionalLight = new THREE.DirectionalLight( 0xFFEEDD );
-    directionalLight.position.set( -1, 1, 1 ).normalize();
-    scene.add( directionalLight );
+   directionalLight = new THREE.DirectionalLight( 0xFFEEDD );
+   directionalLight.position.set( -1, 1, 1 ).normalize();
+   scene.add( directionalLight );
 
     //加载mmd模型
     var onProgress = function ( xhr ) {
-        if ( xhr.lengthComputable ) {
-            var percentComplete = xhr.loaded / xhr.total * 100;
+    	if ( xhr.lengthComputable ) {
+    		var percentComplete = xhr.loaded / xhr.total * 100;
         //    console.log( Math.round(percentComplete, 2) + '% downloaded' );
-        }
-    };
+    }
+};
 
-    var onError = function ( xhr ) {
-    };
-    helper = new THREE.MMDHelper();
-    var loader = new THREE.MMDLoader();
-    loader.load( 'blackmiku/blackmiku.pmx', ['jljt.vmd'], function ( object ) {
-   
-        mesh = object;
-        mesh.position.y = -10;
-        scene.add( mesh );
-        for ( var i = 0, il = mesh.material.length; i < il; i ++ ) {
-            var material = mesh.material[ i ];
-            material.emissive.multiplyScalar( 0.2 );
-        }
-        helper.add( mesh );
-        helper.setAnimation( mesh );
-            audio.play();
-    }, onProgress, onError );
+var onError = function ( xhr ) {
+};
+helper = new THREE.MMDHelper();
+var loader = new THREE.MMDLoader();
+loader.load( 'blackmiku/blackmiku.pmx', ['hifiraver.vmd'], function ( object ) {
+	
+	mesh = object;
+	mesh.position.y = -10;
+	scene.add( mesh );
+	for ( var i = 0, il = mesh.material.length; i < il; i ++ ) {
+		var material = mesh.material[ i ];
+		material.emissive.multiplyScalar( 0.2 );
+	}
+	helper.add( mesh );
+	helper.setAnimation( mesh );
+	audio.play();
+}, onProgress, onError );
 
-    renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
     renderer.setPixelRatio( window.devicePixelRatio );//防变形
     renderer.setSize( windowWidth, windowHeight );
     container.appendChild( renderer.domElement );
 }
 function animate() {
-    requestAnimationFrame( animate );
-    var delta = clock.getDelta();
-    render(delta);
+	requestAnimationFrame( animate );
+	var delta = clock.getDelta();
+	render(delta);
 }
 
 function render(delta) {
 
-    if( mesh && play ) {                
+	if( mesh && play ) {                
         /*
          * 将getDelta的调用放在if判定的外部很重要哦
          * */
